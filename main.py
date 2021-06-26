7089273432757c043a6e34c02284ebeb5a85c5b9
Lee <lees_taylor_18@yahoo.com>
2021-06-26 15:24:52 +0100

    Increased testing accuracy from ~80% to ~94% * Changed label arrays to contain 0.99 instead of actual label * Label arrays contain 0.01 instead of 0 * Added comments to data loading section

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras import optimizers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
 import numpy as np
 
 np.random.seed(1)
 
 # Load data
-# 785 chars per image 0th-i = label
 with open("mnist_train.csv", 'r') as f:
     data = f.readlines()
 
 # load csv data into respective arrays
 labels, images = [], []
 for i,s in enumerate(data):
-    # images
+    # seperate elements by comma and remove \n char
     s = s.split(',')
     s[784] = s[784].replace('\n', '')
-    labels.append([0 for x in range(10)])
-    labels[i][int(s[0])] = int(s[0])
+    # append image of number to array images
     images.append(s[1:785])
+    # create label/Y arrays
+    labels.append([0.01 for x in range(10)])
+    labels[i][int(s[0])] = 0.99
 
 # convert arrays to numpy array
 train_X = np.asarray(images, dtype='int16')
-train_y = np.asarray(labels, dtype='int16')
+train_y = np.asarray(labels, dtype='float32')
 
 with open("mnist_test.csv", 'r') as f:
     data = f.readlines()
@@ -31,27 +33,29 @@ with open("mnist_test.csv", 'r') as f:
 # load csv data into respective arrays
 labels, images = [], []
 for i,s in enumerate(data):
-    # images
+    # seperate elements by comma and remove \n char
     s = s.split(',')
     s[784] = s[784].replace('\n', '')
-    labels.append([0 for x in range(10)])
-    labels[i][int(s[0])] = int(s[0])
+    # append image of number to array images
     images.append(s[1:785])
+    # create label/Y arrays
+    labels.append([0.01 for x in range(10)])
+    labels[i][int(s[0])] = 0.99
 
 # convert arrays to numpy arrays
 test_X = np.asarray(images, dtype='int16')
-test_y = np.asarray(labels, dtype='int16')
+test_y = np.asarray(labels, dtype='float32')
 
 # model layers and build
 model = Sequential()
 model.add(Dense(units=784, activation='sigmoid', input_dim=784))
-model.add(Dense(units=250, activation='sigmoid'))
+model.add(Dense(units=500, activation='sigmoid'))
 model.add(Dense(units=10, activation='sigmoid'))
-opt = optimizers.gradient_descent_v2.SGD()
-model.compile(loss='mean_squared_error', optimizer=opt,
+#opt = optimizers.gradient_descent_v2.SGD()
+model.compile(loss='mean_squared_error', optimizer='adam',
               metrics=['accuracy'])
 
 # fit and evaluate model
-model.fit(train_X, train_y, epochs=10, verbose=1, batch_size=16)
+model.fit(train_X, train_y, batch_size=16, epochs=5, verbose=1)
 print()
-print(f"\nTest Accuracy: {round(model.evaluate(test_X, test_y)[1] * 100, 3)}%")
+print(f"\nTest Accuracy: {round(model.evaluate(test_X, test_y)[1] * 100, 4)}%")
